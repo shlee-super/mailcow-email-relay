5dec92fe67372ccaf2524575afdc1c4a57d510ae
shlee-super <shlee.super@gmail.com>
2025-04-07 18:30:20 +0900

    fix local test script

--- send-invoice.sh
+++ send-invoice.sh
@@ -1,21 +1,13 @@
-#!/bin/bash
+#!/bin/zsh
 
-# 사용법 안내
-if [ "$#" -ne 1 ]; then
-  echo "Usage: $0 <API_BASE_URL>"
-  echo "Example: $0 http://localhost"
-  echo "         $0 https://smtp.superlearn.ing"
-  exit 1
-fi
+API_KEY="furhop-2sasqo-qabpAd"
+API_URL="https://relay.superlearn.ing/send-invoice"
 
-API_URL="$1/send-invoice"
-
-curl -X POST "$API_URL" \
+curl -X POST $API_URL \
+  -H "x-api-key: $API_KEY" \
   -H "Content-Type: application/json" \
-  -H "x-api-key: furhop-2sasqo-qabpAd" \
   -d '{
-    "to": "shlee@superlearn.ing",
-    "subject": "테스트 청구서",
-    "html": "<b>10,000원 청구되었습니다.</b>"
+    "to": "shlee.super@gmail.com",
+    "subject": "Invoice Test",
+    "html": "<h1>Invoice Here</h1><p>This is your invoice.</p>"
   }'
-
